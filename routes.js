d1b307673d31322b21319999d666a230754d86e9
Drulac <dev.drulac@gmail.com>
2016-10-26 12:43:19 +0200

    routage basique operationnel

--- routes.js
+++ routes.js
@@ -1,1 +1,51 @@
- 
+module.exports = function(drudru){
+	let self = this;
+	drudru.routes = self;
+
+	let http = require('http');
+	let url = require('url');
+	let contentType = require('content-type');
+	let mime = require('mime');
+
+	let routes = [];
+
+	let response = function(req, res) {
+
+		let headerContentType = null;
+
+		try{
+			headerContentType = contentType.parse(req);
+		}catch(e){
+			headerContentType = {"Content-Type": "text/plain"};
+		}
+
+		res.writeHead(200, headerContentType);
+
+		var page = url.parse(req.url).pathname;
+		console.log(page);
+
+		for(let route of routes)
+		{
+			if(page == route.path)
+			{
+				route.method().then(function(retour){
+					res.write(retour);
+					res.end();
+				});
+				break;
+			}
+		}
+	};
+
+	self.get = function(path, method){
+		routes.push({path: path, method: method})
+	};
+	
+	var server = http.createServer(response);
+
+	self.listen = function(port){
+		server.listen(port);
+	};
+
+	return self;
+};
